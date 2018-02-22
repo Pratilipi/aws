d81912cff3e100fa573ac95fa4f45074be5519ad
Giridhar <samanth.giridhar@gmail.com>
2018-02-22 15:51:04 +0530

    Prototype setup

--- server.js
+++ server.js
@@ -1,15 +1,51 @@
-'use strict';
+var express = require('express')
+var addRequestId = require('express-request-id')();
+var logger = require('./lib/logger');
 
-const express = require('express');
 
-// Constants
-const PORT = 8080;
+var app = express();
 
-// App
-const app = express();
-app.get('/', function (req, res) {
-  res.send('Hello world\n');
+app.use(addRequestId);
+
+var testController = require('./TestController');
+var testService    = require('./TestService'); 
+
+/*
+app.get('/health', function (req, res){
+	console.log('health');
+        res.send('OK');
 });
 
-app.listen(PORT);
-console.log('Running on http://localhost:' + PORT);
+app.get('/hello-node/one', function (req, res) {
+  res.send('Hello world');
+});
+*/
+
+app.use(logger.logger('Hello-Node'));
+
+app.use('/', function (req,resp,next) {
+	const start = process.hrtime();
+	req.params.start = start;
+	//console.log(req.id, 'start', start);
+	logger.info(req.id+', Start: '+start);
+	next();
+});
+
+
+app.get('/health', function (req, res){
+        const end = process.hrtime(req.params.start);
+        //res.status(200).send("OK");
+        logger.info(req.id+', End: '+end);
+	res.status(200).send("OK");
+});
+
+
+app.use('/hello-node',new testController(new testService()).router);
+
+/*
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+*/
+ 
+app.listen(8080);
